368eb80c07c8f390a6be83a77718531a96449cd8
Sean Reifschneider <jafo@tummy.com>
2012-01-01 08:46:31 -0700

    Changing indentation to spaces instead of tabs.

--- nabmodel.py
+++ nabmodel.py
@@ -12,76 +12,76 @@ from sqlalchemy.orm import relationship, backref
 
 
 class Metadata(Base):
-	'''Global information about the installation.
-	There is only one row in this table.
-	'''
+    '''Global information about the installation.
+    There is only one row in this table.
+    '''
 
-	__tablename__ = 'config'
-	id = Column(Integer, primary_key = True)
-	database_version = Column(Integer)
+    __tablename__ = 'config'
+    id = Column(Integer, primary_key = True)
+    database_version = Column(Integer)
 
-	def __init__(self):
-		self.id = 1
-		self.database_version = 1
+    def __init__(self):
+        self.id = 1
+        self.database_version = 1
 
-	def __repr__(self):
-		return '<Config(id=%s, dbver=%s)>' % ( self.id, self.database_version )
+    def __repr__(self):
+        return '<Config(id=%s, dbver=%s)>' % ( self.id, self.database_version )
 
-	@classmethod
-	def get(cls, session):
-		'''Return an instance of the meta-data record.
-		There is only one meta-data record, this function returns it.
+    @classmethod
+    def get(cls, session):
+        '''Return an instance of the meta-data record.
+        There is only one meta-data record, this function returns it.
 
-		:param session: Database session instance, used to access the Metadata.
-		'''
-		return session.query(Metadata).filter(Metadata.id == 1)[0]
+        :param session: Database session instance, used to access the Metadata.
+        '''
+        return session.query(Metadata).filter(Metadata.id == 1)[0]
 
 
 class Host(Base):
-	'''A backed-up host.
+    '''A backed-up host.
 
-	.. py:attribute:: hostname
+    .. py:attribute:: hostname
 
-	Name of the host to be backed up.  This is also used in the rsync
-	command-line, so it needs to be the name that SSH knows the host as.
-	'''
+    Name of the host to be backed up.  This is also used in the rsync
+    command-line, so it needs to be the name that SSH knows the host as.
+    '''
 
-	__tablename__ = 'hosts'
-	id = Column(Integer, primary_key = True)
-	hostname = Column(String)
+    __tablename__ = 'hosts'
+    id = Column(Integer, primary_key = True)
+    hostname = Column(String)
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def __repr__(self):
-		return '<Host(%s)>' % ( self.hostname, )
+    def __repr__(self):
+        return '<Host(%s)>' % ( self.hostname, )
 
 
 class HostConfig(Base):
-	'''Configurable values for the backed-up hosts.
-	A :py:class:HostConfig that does not reference a :py:class:Host is a
-	global configuration, which other hosts use if they have not overridden
-	those values.
+    '''Configurable values for the backed-up hosts.
+    A :py:class:HostConfig that does not reference a :py:class:Host is a
+    global configuration, which other hosts use if they have not overridden
+    those values.
 
-	.. py:attribute:: host
+    .. py:attribute:: host
 
-	Reference to the :py:class:Host: that this record corresponds to, or None
-	for the default configuration record.
+    Reference to the :py:class:Host: that this record corresponds to, or None
+    for the default configuration record.
 
-	.. py:attribute:: alerts_mail_address
+    .. py:attribute:: alerts_mail_address
 
-	E-mail address to send messages to when there is something that needs
-	human attention.
-	'''
+    E-mail address to send messages to when there is something that needs
+    human attention.
+    '''
 
-	__tablename__ = 'host_configs'
-	id = Column(Integer, primary_key = True)
-	host_id = Column(Integer, ForeignKey('hosts.id'))
-	host = relationship(Host, order_by = id, backref = 'configs')
-	errors_mail_address = Column(String)
+    __tablename__ = 'host_configs'
+    id = Column(Integer, primary_key = True)
+    host_id = Column(Integer, ForeignKey('hosts.id'))
+    host = relationship(Host, order_by = id, backref = 'configs')
+    errors_mail_address = Column(String)
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def __repr__(self):
-		return '<HostConfig(%s)>' % ( self.id, )
+    def __repr__(self):
+        return '<HostConfig(%s)>' % ( self.id, )
