cc235bc4046201ef7df3f98518a9a608e68be3a4
Sean Reifschneider <jafo@tummy.com>
2012-01-09 02:55:52 -0700

    Adding some thoughts on the Storage API.

--- lib/nabstorageplugins/hardlinks.py
+++ lib/nabstorageplugins/hardlinks.py
@@ -6,3 +6,14 @@
 
 class Storage:
     pass
+
+#  methods that are needed:
+#    get host backup root directory
+#    create host
+#    destroy host
+#    create snapshot
+#    destroy snapshot
+#    mount snapshot
+#    unmount snapshot
+#    get storage usage
+#    get host usage
